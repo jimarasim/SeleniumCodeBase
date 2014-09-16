a307f987907e3e3ad39b3ac8d2be306c9ce4088c
jimarasim <jaemzware@hotmail.com>
2014-09-16 06:25:17 -0700

    update node

--- SeleniumGrid/Launch_Selenium_Grid_SAFARI_Node.sh
+++ SeleniumGrid/Launch_Selenium_Grid_SAFARI_Node.sh
@@ -1,1 +1,1 @@
-java -jar selenium-server-standalone-2.42.2.jar -role node -browser platform=MAC,browserName=safari,maxInstances=5,version=7,seleniumProtocol=WebDriver,acceptSslCerts=true -hub http://64.90.58.161:4444/grid/register -host 70.56.87.80 -port 5551
+java -jar selenium-server-standalone-2.42.2.jar -role node -browser platform=MAC,browserName=safari,maxInstances=5,version=7,seleniumProtocol=WebDriver,acceptSslCerts=true -hub http://64.90.58.161:4444/grid/register -host 97.113.113.229 -port 5551
