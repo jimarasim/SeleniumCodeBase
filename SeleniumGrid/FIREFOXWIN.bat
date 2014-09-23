97f7371ca48d66222617dae5cf2400fb591092b3
james arasim <jaemzware@hotmail.com>
2014-09-22 21:31:27 -0700

    update to windows batch files

--- SeleniumGrid/FIREFOXWIN.bat
+++ SeleniumGrid/FIREFOXWIN.bat
@@ -1,1 +1,1 @@
-start cmd /k java -jar selenium-server-standalone-2.43.1.jar -role node -browser "browserName=firefox,maxInstances=5" -hub http://jaemzware.com:4444/grid/register -host 71.35.180.98 -port 5558
+start cmd /k java -jar selenium-server-standalone-2.43.1.jar -role node -browser "browserName=firefox,maxInstances=5" -hub http://jaemzware.com:4444/grid/register -host 71.35.179.176 -port 5558
--- SeleniumGrid/IE11.bat
+++ SeleniumGrid/IE11.bat
@@ -1,1 +1,1 @@
-start cmd /k java -Dwebdriver.ie.driver=IEDriverServer.exe -jar selenium-server-standalone-2.43.1.jar -role node -browser "browserName=InternetExplorer,version=11,maxInstances=5" -hub http://jaemzware.com:4444/grid/register -host 71.35.180.98 -port 5556
+start cmd /k java -Dwebdriver.ie.driver=IEDriverServer.exe -jar selenium-server-standalone-2.43.1.jar -role node -browser "browserName=InternetExplorer,version=11,maxInstances=5" -hub http://jaemzware.com:4444/grid/register -host 71.35.179.176 -port 5556
