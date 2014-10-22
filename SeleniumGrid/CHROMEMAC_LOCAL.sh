33d7f18946c8eb078fd9aa89c5749b56ce6e5c7e
James Arasim <jaemzware@hotmail.com>
2014-10-21 20:40:50 -0700

    new script for local chrome

---
+++ SeleniumGrid/CHROMEMAC_LOCAL.sh
@@ -0,0 +1,1 @@
+java -Dwebdriver.chrome.driver=chromedrivermac -jar selenium-server-standalone-2.43.1.jar -role node -browser "browserName=chrome,maxInstances=5" -hub http://localhost:4444/grid/register -host -port 5561
