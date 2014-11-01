91a0a1b678f6446d886942042a5878c8c51df256
James Arasim <jaemzware@hotmail.com>
2014-11-01 16:34:39 -0700

    new environment type

--- src/main/java/com/jaemzware/seleniumcodebase/EnvironmentType.java
+++ src/main/java/com/jaemzware/seleniumcodebase/EnvironmentType.java
@@ -6,5 +6,5 @@ package com.jaemzware.seleniumcodebase;
  * @author arasim
  */
 public enum EnvironmentType {
-    dev ,test,stage, prod, other, craigslist, ebay, tnaboard, backpage
+    craigslist, ebay, tnaboard, backpage, google
 }
