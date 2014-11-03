18070273b022023260e8c3da271f0f3e3a500e64
jimarasim <jaemzware@hotmail.com>
2014-11-02 22:08:41 -0800

    new safari script for node pointing to static ip server

---
+++ SeleniumGrid/SAFARI_10.1.10.154.sh
@@ -0,0 +1,1 @@
+java -jar selenium-server-standalone-2.44.0.jar -role node -browser platform=MAC,browserName=safari,maxInstances=5,version=7,seleniumProtocol=WebDriver,acceptSslCerts=true -hub http://10.1.10.154:4444/grid/register -port 5553
