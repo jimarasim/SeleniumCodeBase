7171ff79b86e03f17032852d5dfe1efe1f9b62e7
jimarasim <jaemzware@hotmail.com>
2014-12-18 19:46:29 -0800

    decreased hardcoded sleep timeout

--- src/main/java/com/jaemzware/seleniumcodebase/CodeBase.java
+++ src/main/java/com/jaemzware/seleniumcodebase/CodeBase.java
@@ -84,7 +84,7 @@ public class CodeBase {
     // default time IN SECONDS to wait when finding elements
     protected int defaultImplicitWait = 60;
     protected static final int quickWaitMilliSeconds = 5000;
-    protected static final int waitForPageLoadMilliSeconds = 20000;
+    protected static final int waitForPageLoadMilliSeconds = 15000;
 
     // verification errors that can occur during a test
     protected StringBuilder verificationErrors = new StringBuilder();
