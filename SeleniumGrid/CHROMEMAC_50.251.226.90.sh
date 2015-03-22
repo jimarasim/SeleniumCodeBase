fac07178d8180a6c59af80553a470f55724b2e8e
James Arasim <jaemzware@hotmail.com>
2015-03-22 15:20:43 -0700

    updated grid jar

--- SeleniumGrid/CHROMEMAC_50.251.226.90.sh
+++ SeleniumGrid/CHROMEMAC_50.251.226.90.sh
@@ -1,1 +1,1 @@
-java -Dwebdriver.chrome.driver=chromedrivermac -jar selenium-server-standalone-2.44.0.jar -role node -browser platform=MAC,browserName=chrome,maxInstances=5,seleniumProtocol=WebDriver,acceptSslCerts=true -hub http://50.251.226.90:4445/grid/register -host 166.171.123.245 -port 5561
+java -Dwebdriver.chrome.driver=chromedrivermac -jar selenium-server-standalone-2.45.0.jar -role node -browser platform=MAC,browserName=chrome,maxInstances=5,seleniumProtocol=WebDriver,acceptSslCerts=true -hub http://50.251.226.90:4445/grid/register -host 166.171.123.245 -port 5561
--- SeleniumGrid/SAFARI_LOCAL.sh
+++ SeleniumGrid/SAFARI_LOCAL.sh
@@ -1,1 +1,1 @@
-java -jar selenium-server-standalone-2.44.0.jar -role node -browser platform=MAC,browserName=safari,maxInstances=5,version=7,seleniumProtocol=WebDriver,acceptSslCerts=true -hub http://localhost:4444/grid/register -port 5553
+java -jar selenium-server-standalone-2.45.0.jar -role node -browser platform=MAC,browserName=safari,maxInstances=5,version=7,seleniumProtocol=WebDriver,acceptSslCerts=true -hub http://localhost:4444/grid/register -port 5553
