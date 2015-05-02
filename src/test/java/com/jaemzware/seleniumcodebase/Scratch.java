fe156fcee331b77b086730c2f202912b0c4a47ef
jimarasim <jaemzware@hotmail.com>
2015-05-02 07:42:32 -0700

    had to move links above footer of html file, duh

--- src/test/java/com/jaemzware/seleniumcodebase/Scratch.java
+++ src/test/java/com/jaemzware/seleniumcodebase/Scratch.java
@@ -252,10 +252,12 @@ public class Scratch extends CodeBase {
             }
 
         //COMPLETE WRITING REPORT WEB PAGE
-            writer.println(HtmlReportFooter());
+            
             System.out.println("INDEX FILE WRITTEN:" + fileName);
             System.out.println("INTERNAL COPY: http://10.1.10.156/jenkinsArtifacts/"+ fileName);
             System.out.println("EXTERNAL COPY: http://jaemzware.com/jenkinsArtifacts/"+fileName);
+            
+            writer.println(HtmlReportFooter());
             
 
         } catch (Exception ex) {
