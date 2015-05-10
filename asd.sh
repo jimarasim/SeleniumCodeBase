e7ab32a11baa35b734a903edaf6fe7253dbb4975
JAMES ARASIM <jaemzware@hotmail.com>
2015-05-09 22:37:33 -0600

    ios verifylogos codepath

--- asd.sh
+++ asd.sh
@@ -20,4 +20,4 @@
 
 
 
-mvn -Dtest=Scratch#VerifyLogos -DappiumUdid=88ff683cec637c3f1279386620b5397d48bc8341 -DappiumIosDeviceName=iJaemzware -DappiumIosTargetVersion=8.3 -DaNumber=0 -Dbrowser=APPIUMSAFARIDEVICE -Dinput=https://seattlerules.com -Duserid=seattlerules.com -DaString=//a[@title='Seattle Rules'] -DaHubPort=4723 -DaHubServer=localhost test
+mvn -Dtest=Scratch#VerifyLogosIos -DappiumUdid=88ff683cec637c3f1279386620b5397d48bc8341 -DappiumIosDeviceName=iJaemzware -DappiumIosTargetVersion=8.3 -DaNumber=0 -Dbrowser=APPIUMSAFARIDEVICE -Dinput=https://seattlerules.com -Duserid=seattlerules.com -DaString=//a[@title='Seattle Rules'] -DaHubPort=4723 -DaHubServer=localhost test
--- src/test/java/com/jaemzware/seleniumcodebase/Scratch.java
+++ src/test/java/com/jaemzware/seleniumcodebase/Scratch.java
@@ -277,6 +277,195 @@ public class Scratch extends CodeBase {
     }
 
     @Test
+    public void VerifyLogosIos() {
+
+        // create a file for the web page log
+        String fileName = "index.htm"; //        String fileName = "Index-VerifyLogos-" + getDateStamp() + ".htm";
+        PrintWriter writer = null;
+        String fileWriteString;
+
+        try {
+            // open browser
+            StartAppiumDriver();
+            
+            if(driver==null){
+                throw new Exception("DRIVER WAS NOT SET; SUITABLE DRIVER WAS NOT FOUND.  LOOK ABOVE FOR ISSUES REPORTED BY StartDrvier()");
+            }
+            
+            if(!browser.toString().contains("APPIUM")){
+                //set implicit wait
+                driver.manage().timeouts().implicitlyWait(defaultImplicitWait, TimeUnit.SECONDS);
+            }
+
+        //GET PARAMETERS
+            // get START url
+            String starturl = new String();
+            if (input != null && !input.isEmpty()) {
+                starturl = input;
+            } else {
+                throw new Exception("START URL NOT SPECIFIED (-Dinput)");
+            }
+
+            // get base url
+            String baseurl = new String();
+            if (userid != null && !userid.isEmpty()) {
+                baseurl = userid;
+            } else {
+                throw new Exception("BASE URL NOT SPECIFIED (-Duserid)");
+            }
+
+            // get xpath to look for
+            String logoxpath = new String();
+            if (aString != null && !aString.isEmpty()) {
+                logoxpath = aString;
+            } else {
+                throw new Exception("LOGOXPATH NOT SPECIFIED (-DaString");
+            }
+            
+        //CREATE A REPORT WEB PAGE
+            // create the web page
+            writer = new PrintWriter(fileName, "UTF-8");
+
+            // write the html header in the web page
+            writer.println(HtmlReportHeader("jaemzware-verifylogos [baseurl:"+baseurl+" starturl:"+starturl+" logoxpath:"+logoxpath+"]"));
+            
+        // NAVIGATE TO THE STARTING PAGE
+            System.out.println("STARTURL:"+starturl);
+            fileWriteString = driverGetWithTime(starturl);
+           
+            // write stats to html report
+            writer.println(fileWriteString);
+
+        //LOGGING
+            if(System.getProperty("logging")==null){
+                System.out.println("LOGGING DISABLED - USE -Dlogging TO SEE BROWSER ERRORS AND WARNINGS");
+            } 
+            else if(browser.toString().contains("APPIUM")){
+                System.out.println("LOGGING NOT SUPPORTED WITH APPIUM");
+            }
+            else {
+                writer.println(ExtractJSLogs());
+            }
+            
+            // verify logo
+            System.out.println("VERIFYING LOGO AT:"+logoxpath+" ON:"+starturl);
+            writer.println(VerifyXpathOnCurrentPage(logoxpath));
+
+        //GET HREFS
+            // get all non-empty/non-javascript href on the page that contain the baseurl
+            Map<String, String> hrefs = new HashMap<>();
+            String hrefFound;
+
+            //getting all internal hrefs
+            System.out.println("GETTING ALL INTERNAL ANCHORS MATCHING XPATH:"+linksOnSplashPageXpath+" ON:"+starturl);
+                        
+            if (IsElementPresent(By.xpath(linksOnSplashPageXpath))) {
+                
+                System.out.println("FINDING ANCHOR ELEMENTS");
+                List<WebElement> internalAnchors = driver.findElements(By.xpath(linksOnSplashPageXpath));
+                
+                if(internalAnchors.size()<1){
+                    throw new Exception("NO LINKS FOUND AT XPATH:"+linksOnSplashPageXpath+" ON PAGE:"+starturl);
+                }
+                
+                for (WebElement we : internalAnchors) {
+                    try {
+                        hrefFound = we.getAttribute("href");
+                    } catch (Exception ex) {
+                        System.out.println("WARNING: EXCEPTION GETTING HREF FROM LIST.");
+                        writer.println("<span class='warning'>WARNING: EXCEPTION GETTING HREF FROM LIST</span>");
+                        break;
+                    }
+
+                    // only get hrefs that contain the base url, and html like pages
+                    if (hrefFound.toLowerCase().contains(baseurl) && 
+                            !hrefFound.toLowerCase().contains("feed") &&
+                            !hrefFound.toLowerCase().contains("rss") &&
+                            !hrefFound.toLowerCase().contains("javascript") &&
+                            !hrefFound.toLowerCase().contains(".jpg") &&
+                            !hrefFound.toLowerCase().contains(".jpeg") &&
+                            !hrefFound.toLowerCase().contains(".png") &&
+                            !hrefFound.toLowerCase().contains(".gif")) {
+                        hrefs.put(hrefFound, starturl);
+                        System.out.println("WILL VISIT:" + hrefFound);
+                    } else {
+                        System.out
+                                .println("SKIPPING: URL:" + hrefFound+" ON:"+starturl + " DOES NOT CONTAIN BASE URL:" + baseurl);
+                    }
+
+                }
+            } else {
+                System.out.println("WARNING: NO LINKS FOUND ON PAGE MATCHING XPATH:" + linksOnSplashPageXpath+" ON:"+starturl);
+            }
+
+        //VISIT HREFS
+            // visit each href, report load time, and make sure the page has the logo
+            int maxVisits = (aNumber!=null||!aNumber.isEmpty())?Integer.parseInt(aNumber):0; //check if the max number was specified
+            int visitCount = 0;
+            
+            System.out.println("VISITING HREFS FOUND AT XPATH:"+linksOnSplashPageXpath+" ON:"+starturl);
+            for (String href : hrefs.keySet()) {
+
+                // make sure we're on a real page, and not an image
+                if (    href.endsWith(".jpg") || 
+                        href.endsWith(".gif") || 
+                        href.endsWith("rss2")) {
+                    continue;
+                }
+                
+                String oldUrl=driver.getCurrentUrl();
+            
+                // go to the href
+                fileWriteString = driverGetWithTime(href);
+                
+                // write stats to html report
+                writer.println(fileWriteString);
+                
+        //ERROR LOGGING - TAKES LONG - ADD CAPABILITY WHEN CREATING driver BEFORE USING
+                if(System.getProperty("logging")==null || 
+                        browser.toString().contains("APPIUM") ){
+                } else {
+                    System.out.println("WRITING OUT LOGS FOR:"+href);
+                    writer.println(ExtractJSLogs());
+                }
+                
+                System.out.println("VERIFYING LOGO AT:"+logoxpath+" ON:"+href);
+                writer.println(VerifyXpathOnCurrentPage(logoxpath));
+                
+                
+                //check the desired image count, and break if it's been reached
+                if((maxVisits>0) && (++visitCount>maxVisits-1)){
+                    break;
+                }
+                
+            }
+
+        //COMPLETE WRITING REPORT WEB PAGE
+            
+            System.out.println("INDEX FILE WRITTEN:" + fileName);
+            System.out.println("INTERNAL COPY: http://10.1.10.156/jenkinsArtifacts/"+ fileName);
+            System.out.println("EXTERNAL COPY: http://jaemzware.com/jenkinsArtifacts/"+fileName);
+            
+            writer.println(HtmlReportFooter());
+            
+
+        } catch (Exception ex) {
+            ScreenShot();
+            System.out.println("VERIFY LOGOS EXCEPTION:"+ex.getMessage());
+//            CustomStackTrace("VerifyLogos EXCEPTION", ex);
+//            Assert.fail(ex.getMessage());
+        } finally {
+            
+            //WRITE THE FILE
+            // write the file if it was created
+            if (writer != null) {
+                writer.flush();
+                writer.close();
+            }
+        }
+    }
+    
+    @Test
     public void CheckGmail() {
         // uses app password for gmail. app name "Scratch" https://support.google.com/accounts/answer/185833
         try {
