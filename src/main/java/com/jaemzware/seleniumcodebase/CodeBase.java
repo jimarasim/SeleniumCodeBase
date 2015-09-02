442a9815244289bb786d88e0536b00e57648aff5
jimarasim <jaemzware@hotmail.com>
2015-09-01 19:17:29 -0700

    change from imac

--- src/main/java/com/jaemzware/seleniumcodebase/CodeBase.java
+++ src/main/java/com/jaemzware/seleniumcodebase/CodeBase.java
@@ -94,8 +94,8 @@ public class CodeBase {
     protected static String noScroll = null;
     
     // jenkins report folder url
-    protected static final String jenkinsReportPath = "http://jaemzware.org:8081/";
-    protected static final String jenkinsReportPathInternal = "http://10.1.10.156:8081/";
+    protected static final String jenkinsReportPath = "http://jaemzware.org/";
+    protected static final String jenkinsReportPathInternal = "http://10.1.10.156/";
     protected static final String jenkinsDeployDirectory = "jaemzwareArtifacts";
     
     // default time IN SECONDS to wait when finding elements
