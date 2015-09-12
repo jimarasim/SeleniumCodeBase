cedb5f6fd32e05d176c134e47f61d60c69bf3eb6
jimarasim <jaemzware@hotmail.com>
2015-09-12 08:01:15 -0700

    added a script for comcast

---
+++ comcastnogrid.sh
@@ -0,0 +1,1 @@
+mvn -Dtest=Scratch#VerifyLogos -Dbrowser=CHROME -Dinput=http://business.comcast.com/ -DaNumber=0 -Dreport=local -Duserid=business.comcast.com -DaString="//p" -DwaitAfterPageLoadMilliSeconds=0 -Dlogging -Dnogrid test
