6efc23f9866c090a4e51d30fc2687e949411190f
JAMES ARASIM <jaemzware@hotmail.com>
2016-06-23 11:27:09 -0700

    added DdefaultImplicitWaitSeconds to bpt script

--- nogridchromemacbrownpapertickets.sh
+++ nogridchromemacbrownpapertickets.sh
@@ -2,4 +2,6 @@
 # -Dinput page to start at
 # -Duserid string contained in links of href to follow
 # -DaString xpath of logo to verify
-mvn -Dtest=Scratch#VerifyLogos -Dbrowser=CHROME -Dinput="http://brownpapertickets.com" -DaNumber=3 -Dreport=verifylogosBPT -Duserid=brownpapertickets -DaString="/html/body/div[1]/table[1]//img" -DwaitAfterPageLoadMilliSeconds=0 -Dlogging -Dnogrid test
+# -DaNumber maxiumum number of pages to visit. set to 0 for all
+# -DdefaultImplicitWaitSeconds how long to wait while trying to locate elements
+mvn -Dtest=Scratch#VerifyLogos -Dbrowser=CHROME -Dinput="http://brownpapertickets.com" -DaNumber=3 -Dreport=verifylogosBPT -Duserid=brownpapertickets -DaString="/html/body/div[1]/table[1]//img" -DwaitAfterPageLoadMilliSeconds=0 -DdefaultImplicitWaitSeconds=60 -Dlogging -Dnogrid test
