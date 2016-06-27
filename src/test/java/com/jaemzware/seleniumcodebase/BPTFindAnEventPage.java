b780bfdd1910fe3d5f2bcfd8ef40c58fb2ff7f80
JAMES ARASIM <jaemzware@hotmail.com>
2016-06-27 15:10:00 -0700

    removed a blank line is all

--- src/test/java/com/jaemzware/seleniumcodebase/BPTFindAnEventPage.java
+++ src/test/java/com/jaemzware/seleniumcodebase/BPTFindAnEventPage.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.By;
  * Created by jameskarasim on 6/26/16.
  */
 public class BPTFindAnEventPage {
-
     public String pageUrl = "https://tony.bpt.me/browse.html";
     public By pageTitle  = By.xpath("//title[contains(text(),'Brown Paper Tickets - The fair-trade ticketing company.')]");
     public By pageHeader = By.xpath("//*[contains(text(),'The fair-trade ticketing company.')]");
