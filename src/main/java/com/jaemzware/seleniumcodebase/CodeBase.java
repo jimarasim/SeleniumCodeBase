9407ab53da5f9fd4f7f71ef44ebacae12de0e211
James Arasim <jaemzware@hotmail.com>
2016-06-29 18:43:35 -0700

    took out some logging that is too noisy and not very useful

--- src/main/java/com/jaemzware/seleniumcodebase/CodeBase.java
+++ src/main/java/com/jaemzware/seleniumcodebase/CodeBase.java
@@ -399,8 +399,8 @@ public class CodeBase {
 
                         LoggingPreferences loggingprefs = new LoggingPreferences();
                         loggingprefs.enable(LogType.BROWSER, Level.ALL);
-                        loggingprefs.enable(LogType.CLIENT, Level.ALL);
-                        loggingprefs.enable(LogType.DRIVER, Level.ALL);
+//                        loggingprefs.enable(LogType.CLIENT, Level.ALL);
+//                        loggingprefs.enable(LogType.DRIVER, Level.ALL);
                         cap.setCapability(CapabilityType.LOGGING_PREFS, loggingprefs);
                         
                         System.out.println("-Dlogging SPECIFIED");
