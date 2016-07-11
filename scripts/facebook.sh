fd8e6618ff87b4fc96209f2c02e995d5661eb800
JAMES ARASIM <jaemzware@hotmail.com>
2016-07-11 10:33:16 -0700

    added employee

--- scripts/facebook.sh
+++ scripts/facebook.sh
@@ -1,1 +1,5 @@
-mvn -Dtest=Facebook#FaceCrawlAllLinks -Dnogrid -Dbrowser=CHROMEMAC -DaNumber=5 -DaString=//p -DdefaultImplicitWaitSeconds=120 -Dinput='https://www.facebook.com/angela.chandler.370' test
+
+cd ../ && mvn -Dtest=Facebook#FaceCrawlAllLinks -Dnogrid -Dbrowser=CHROMEMAC -DaNumber=5 -DaString=//p -DdefaultImplicitWaitSeconds=120 -Dinput='https://www.facebook.com/katherine.johnstenbaugh' test
+#mvn -Dtest=Facebook#FaceCrawlAllLinks -Dnogrid -Dbrowser=CHROMEMAC -DaNumber=5 -DaString=//p -DdefaultImplicitWaitSeconds=120 -Dinput='https://www.facebook.com/angela.chandler.370' test
+
+
