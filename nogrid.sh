9b7130f093b9816b23029497cbd9e24d6c499c6d
JAMES ARASIM <jaemzware@hotmail.com>
2016-09-15 10:38:00 -0700

    some updates

--- nogrid.sh
+++ nogrid.sh
@@ -1,1 +1,1 @@
-mvn -Dtest=Scratch#VerifyLogos -Dbrowser=SAFARI -Dinput=http://starbucks.com -DaNumber=50 -Dreport=local -Duserid=starbucks.com -DaString="//a" -DwaitAfterPageLoadMilliSeconds=0 -Dlogging -Dnogrid test
+mvn -Dtest=Scratch#VerifyLogos -Dbrowser=CHROME -Dinput=http://seattlerules.com -DaNumber=50 -Dreport=local -Duserid=seattlerules.com -DaString="//a[contains(@href,'seattlerules.com')]" -DwaitAfterPageLoadMilliSeconds=0 -Dlogging -Dnogrid test
