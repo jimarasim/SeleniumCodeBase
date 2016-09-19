6b971d1671bc0de45e8c113cd7732fd7b6ca49cd
JAMES ARASIM <jaemzware@hotmail.com>
2016-09-19 09:11:03 -0700

    some script updates

--- nogrid.sh
+++ nogrid.sh
@@ -1,4 +1,4 @@
-mvn -Dtest=Scratch#VerifyLogos -Dinput=http://grindr.com -Duserid=grindr.com -DaString="//a[contains(@href,'grindr.com')] and //img" -Dbrowser=CHROME  -DaNumber=0 -Dreport=grindr -DwaitAfterPageLoadMilliSeconds=0 -Dlogging -Dnogrid -DnoScroll -DnoScreenShots test
+mvn -Dtest=Scratch#VerifyLogos -Dinput=http://grindr.com -Duserid=grindr.com -DaString="//a[contains(@href,'grindr.com')] and //img" -Dbrowser=CHROME  -DaNumber=0 -Dreport=grindr20160915112036 -DwaitAfterPageLoadMilliSeconds=0 -Dlogging -Dnogrid test
 #INPUT MAPS TO STARTURL (eg http://grindr.com)
 #USERID MAPS TO BASEURL OF LINKS TO FOLLOW (eg grindr.com)
 #ASTRING IS XPATH OF PAGE OBJECTS TO COLLECT FROM EACH PAGE (eg xpath //a[contains(@href,'grindr.com')] and //img)
