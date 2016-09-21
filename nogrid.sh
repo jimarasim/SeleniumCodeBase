945f015b1735e2d50007840c0f89bf64b425543b
JAMES ARASIM <jaemzware@hotmail.com>
2016-09-20 19:12:11 -0700

    updated script for nbc again

--- nogrid.sh
+++ nogrid.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-mvn -Dtest=Scratch#VerifyLogos -Dinput=http://grindr.com -Duserid=grindr.com -DaString="//a[contains(@href,'grindr.com')] and //img" -Dbrowser=CHROME  -DaNumber=0 -Dreport=grindr20160915112036 -DwaitAfterPageLoadMilliSeconds=0 -Dlogging -Dnogrid -DnoScroll test
+mvn -Dtest=Scratch#VerifyLogos -Dinput=http://nbc.com -Duserid=nbc.com -DaString="//a[contains(@href,'nbc.com')] and //img" -Dbrowser=CHROME  -DaNumber=0 -Dreport=nbc -DwaitAfterPageLoadMilliSeconds=0 -Dlogging -Dnogrid -DnoScroll test
 #INPUT MAPS TO STARTURL (eg http://grindr.com)
 #USERID MAPS TO BASEURL OF LINKS TO FOLLOW (eg grindr.com)
 #ASTRING IS XPATH OF PAGE OBJECTS TO COLLECT FROM EACH PAGE (eg xpath //a[contains(@href,'grindr.com')] and //img)
