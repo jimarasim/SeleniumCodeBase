a9d4831fc133c786f7dbf25f953c3c61df08117d
JAMES ARASIM <jaemzware@hotmail.com>
2016-12-29 16:59:34 -0800

    changed to test jaemzware.com

--- nogrid.sh
+++ nogrid.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-mvn -Dtest=Scratch#VerifyLogos -Dinput=http://nbc.com -Duserid=nbc.com -DaString="//a[contains(@href,'nbc.com')] and //img" -Dbrowser=FIREFOX  -DaNumber=0 -Dreport=nbc -DwaitAfterPageLoadMilliSeconds=0 -Dlogging -Dnogrid -DnoScroll test
-#INPUT MAPS TO STARTURL (eg http://grindr.com)
+mvn -Dtest=Scratch#VerifyLogos -Dinput=http://jaemzware.com -Duserid=.com -DaString="//img" -Dbrowser=CHROME  -DaNumber=0 -Dreport=jaemzware -DwaitAfterPageLoadMilliSeconds=0 -Dlogging -Dnogrid -DnoScroll test
+#INPUT MAPS TO STARTURL (eg http://jaemzware.com)
 #USERID MAPS TO BASEURL OF LINKS TO FOLLOW (eg grindr.com)
 #ASTRING IS XPATH OF PAGE OBJECTS TO COLLECT FROM EACH PAGE (eg xpath //a[contains(@href,'grindr.com')] and //img)
