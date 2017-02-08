69b01ae9e2a88a4d11974df0154d79df75dfe172
JAMES ARASIM <jaemzware@hotmail.com>
2017-02-08 15:24:24 -0800

    took out facebook automation because it doesn't relate to base clase behavior

--- facebook.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-mvn -Dtest=Facebook#FaceCrawlAllLinks -Dnogrid -Dbrowser=CHROME -DaNumber=3 -Dreport=facebook -DaString=//p -DdefaultImplicitWaitSeconds=5 -Dinput='https://www.facebook.com' test
-
-
-
