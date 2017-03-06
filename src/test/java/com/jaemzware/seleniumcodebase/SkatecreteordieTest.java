2aff3df08ef473da502cdd004324d0ae2a1cadc4
JAMES ARASIM <jaemzware@hotmail.com>
2017-03-06 09:50:31 -0800

    added parameterized test for selecting each state and counting at least 10 parks

--- src/test/java/com/jaemzware/seleniumcodebase/SkatecreteordieTest.java
+++ src/test/java/com/jaemzware/seleniumcodebase/SkatecreteordieTest.java
@@ -1,13 +1,32 @@
 package com.jaemzware.seleniumcodebase;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.Arrays;
 
 /**
  * Created by jameskarasim on 2/22/17.
  */
+@RunWith(Parameterized.class)
 public class SkatecreteordieTest extends BaseTest {
     SkatecreteordiePage testPage = new SkatecreteordiePage();
+
+    String testState = "";
+
+    @Parameterized.Parameters(name = "parameterization on city")
+    public static Iterable<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"OREGON"},
+                {"MONTANA"},
+                {"IDAHO"},
+                {"DENMARK"}
+        });
+    }
 
     @Test
     public void CheckSanityElementsTest() throws Exception
@@ -24,5 +43,28 @@ public class SkatecreteordieTest extends BaseTest {
                 verificationErrors.append("FAIL: MISSING SANITY ELEMENT"+sanityElement.toString());
             }
         }
+    }
+
+    public SkatecreteordieTest(String state){
+        testState=state;
+    }
+
+    @Test
+    public void SelectStateParks() throws Exception
+    {
+        //GO TO THE PAGE FOR PICKING OREGON FROM THE DROPDOWN
+        driverGetWithTime(testPage.getBasePageUrl());
+
+        //SELECT OREGON FROM THE STATE DROP DOWN
+        Select stateDropDown = new Select(driver.findElement(testPage.skateparkStateDropdown));
+        stateDropDown.selectByValue(testState);
+
+        Thread.sleep(500);
+
+        //MAKE SURE THERE ARE AT LEAST 10 PARKS
+        int parkCount = driver.findElements(testPage.skateparkLinks).size();
+
+        Assert.assertTrue(parkCount>10);
+
     }
 }
